c25a77999bcfcc3a6012de3d3c438af02b7aefe3
dairsaber <dairsaber@outlook.com>
2022-10-07 17:50:58 +0800

    feat: 跑通数据库等

--- src/common/database/database.providers.ts
+++ src/common/database/database.providers.ts
@@ -1,6 +1,6 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { getConfig } from 'src/utils/index';
-import path from 'path';
+import * as path from 'path';
 
 // 设置数据库类型
 const databaseType: DataSourceOptions['type'] = 'mongodb';
@@ -9,7 +9,7 @@ const { MONGODB_CONFIG } = getConfig();
 const MONGODB_DATABASE_CONFIG = {
   ...MONGODB_CONFIG,
   type: databaseType,
-  entities: [path.join(__dirname, `../../**/*.${MONGODB_CONFIG.entities}.entity{.ts,.js}`)],
+  entities: [path.join(__dirname, `../../**/*.${MONGODB_CONFIG.entities}.entity{.ts}`)],
 };
 
 const MONGODB_DATA_SOURCE = new DataSource(MONGODB_DATABASE_CONFIG);
